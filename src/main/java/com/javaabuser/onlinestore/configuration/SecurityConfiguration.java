2de33d2ee078ed254e5796a5e0b812de41e7c131
JavaAbuser <steblevskiymaxim@ua.fm>
2022-07-28 19:01:17 +0300

    Added passwordEncoder bean

--- src/main/java/com/javaabuser/onlinestore/configuration/SecurityConfiguration.java
+++ src/main/java/com/javaabuser/onlinestore/configuration/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.javaabuser.onlinestore.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -21,5 +24,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/login")
                 .defaultSuccessUrl("/hello")
                 .failureUrl("/login?error");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
