7c68cbc658f2c4c9b14c178fdd4a133012c7d84a
JavaAbuser <steblevskiymaxim@ua.fm>
2022-07-30 16:25:30 +0300

    View class for JsonView was created

---
+++ src/main/java/com/javaabuser/onlinestore/util/View.java
@@ -0,0 +1,11 @@
+package com.javaabuser.onlinestore.util;
+
+public class View {
+    public static class ViewForLogin{
+
+    }
+
+    public static class ViewForRegistration extends ViewForLogin {
+
+    }
+}
