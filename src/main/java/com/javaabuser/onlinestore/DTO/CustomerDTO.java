16d5c4d39674da55017a51103cd89bc4795ec12b
JavaAbuser <steblevskiymaxim@ua.fm>
2022-07-30 16:17:34 +0300

    Added JsonView annotations

--- src/main/java/com/javaabuser/onlinestore/DTO/CustomerDTO.java
+++ src/main/java/com/javaabuser/onlinestore/DTO/CustomerDTO.java
@@ -1,32 +1,37 @@
 package com.javaabuser.onlinestore.DTO;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.javaabuser.onlinestore.util.View;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 
-public class CustomerLoginDTO {
+public class CustomerDTO {
+    @JsonView(View.ViewForRegistration.class)
+    @NotNull
+    @Max(value = 30)
+    private String name;
+
+    @JsonView(View.ViewForLogin.class)
     @NotNull
     private String password;
 
+    @JsonView(View.ViewForLogin.class)
     @Email
     @NotNull
     @Max(value = 40)
     private String email;
 
-    public CustomerLoginDTO(String email, String password) {
-        this.email = email;
-        this.password = password;
+    public CustomerDTO() {
     }
 
-    public CustomerLoginDTO() {
+    public String getName() {
+        return name;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPassword() {
@@ -35,5 +40,13 @@ public class CustomerLoginDTO {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
