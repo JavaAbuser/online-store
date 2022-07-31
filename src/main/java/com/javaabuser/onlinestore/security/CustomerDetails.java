5b5888fb9e1bc8004c232a9c12fba0037148911c
JavaAbuser <steblevskiymaxim@ua.fm>
2022-07-31 18:55:55 +0300

    Added fields and changed constructor

--- src/main/java/com/javaabuser/onlinestore/security/CustomerDetails.java
+++ src/main/java/com/javaabuser/onlinestore/security/CustomerDetails.java
@@ -1,16 +1,34 @@
 package com.javaabuser.onlinestore.security;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.javaabuser.onlinestore.models.Customer;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomerDetails implements UserDetails {
+    private static final long serialVersionUID = 1L;
     private final Customer customer;
+    private int id;
+    private String name;
+    @JsonIgnore
+    private String password;
+    private String email;
+    private List<? extends GrantedAuthority> authorities;
 
-    public CustomerDetails(Customer customer) {
+    public CustomerDetails (Customer customer) {
         this.customer = customer;
+        this.id = customer.getId();
+        this.name = customer.getName();
+        this.password = customer.getPassword();
+        this.email = customer.getEmail();
+        this.authorities = customer.getRoles().stream().map(
+                role -> new SimpleGrantedAuthority(role.getRole().name())
+        ).collect(Collectors.toList());
     }
 
     @Override
