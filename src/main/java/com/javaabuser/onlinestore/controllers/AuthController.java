a28df81b19108459bbcac487fe549fc5c2490c3d
JavaAbuser <steblevskiymaxim@ua.fm>
2022-08-02 01:24:23 +0300

    AuthController was created

--- src/main/java/com/javaabuser/onlinestore/controllers/AuthController.java
+++ src/main/java/com/javaabuser/onlinestore/controllers/AuthController.java
@@ -1,16 +1,32 @@
 package com.javaabuser.onlinestore.controllers;
 
+import com.javaabuser.onlinestore.models.Customer;
+import com.javaabuser.onlinestore.models.Role;
+import com.javaabuser.onlinestore.models.enums.ERole;
+import com.javaabuser.onlinestore.security.CustomerDetails;
 import com.javaabuser.onlinestore.security.jwt.JwtUtil;
 import com.javaabuser.onlinestore.services.CustomerService;
 import com.javaabuser.onlinestore.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/customer")
@@ -31,8 +47,40 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<HttpStatus> authenticate(){
+    public ResponseEntity<Customer> authenticate(@RequestBody @Valid Customer customer){
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(customer.getName(), customer.getPassword()));
 
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        CustomerDetails customerDetails = (CustomerDetails) authentication.getPrincipal();
+        ResponseCookie jwtCookie = jwtUtil.generateJwtCookie(customerDetails);
+        Set<String> rolesString = customerDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet());
+
+        Set<Role> rolesRole = new HashSet<>();
+        for(String role : rolesString){
+            switch (role){
+                case "CUSTOMER":
+                    rolesRole.add(new Role(ERole.CUSTOMER));
+                    break;
+                case "SELLER":
+                    rolesRole.add(new Role(ERole.SELLER));
+                    break;
+                case "MODERATOR":
+                    rolesRole.add(new Role(ERole.MODERATOR));
+                    break;
+                case "OWNER":
+                    rolesRole.add(new Role(ERole.OWNER));
+                    break;
+            }
+        }
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body(new Customer(customerDetails.getName(),
+                        customerDetails.getPassword(),
+                        customerDetails.getEmail(),
+                        rolesRole));
     }
 
     @PostMapping("/logout")
