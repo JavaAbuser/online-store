01771bcde653e083c4fbb5d6d3890cc8586b98a0
JavaAbuser <steblevskiymaxim@ua.fm>
2022-08-02 00:06:06 +0300

    Added method validateJwtToken

--- src/main/java/com/javaabuser/onlinestore/security/jwt/JwtUtil.java
+++ src/main/java/com/javaabuser/onlinestore/security/jwt/JwtUtil.java
@@ -54,4 +54,20 @@ public class JwtUtil {
     public String getUserNameFromJwtToken(String token) {
         return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
     }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
 }
