6fb6f19edd6f2b55fbd3d6af43b7a05fcba754c5
JavaAbuser <steblevskiymaxim@ua.fm>
2022-08-01 16:14:26 +0300

    Added methods generateJwtCookie and generateTokenFromCustomerName

--- src/main/java/com/javaabuser/onlinestore/security/jwt/JwtUtil.java
+++ src/main/java/com/javaabuser/onlinestore/security/jwt/JwtUtil.java
@@ -1,13 +1,17 @@
 package com.javaabuser.onlinestore.security.jwt;
 
+import com.javaabuser.onlinestore.security.CustomerDetails;
+import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @Component
 public class JwtUtil {
@@ -26,5 +30,19 @@ public class JwtUtil {
         } else {
             return null;
         }
+    }
+
+    public ResponseCookie generateJwtCookie(CustomerDetails customerPrincipal) {
+        String jwt = generateTokenFromCustomerName(customerPrincipal.getUsername());
+        return ResponseCookie.from(jwtCookie, jwt).path("/").httpOnly(true).build();
+    }
+
+    public String generateTokenFromCustomerName(String customerName) {
+        return Jwts.builder()
+                .setSubject(customerName)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
     }
 }
