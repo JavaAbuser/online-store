6983ec72e49b166a32f31653e9acd8add94a736e
JavaAbuser <steblevskiymaxim@ua.fm>
2022-08-01 16:11:01 +0300

    Added annotation @Value

--- src/main/java/com/javaabuser/onlinestore/security/jwt/JwtUtil.java
+++ src/main/java/com/javaabuser/onlinestore/security/jwt/JwtUtil.java
@@ -2,6 +2,7 @@ package com.javaabuser.onlinestore.security.jwt;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.WebUtils;
 
@@ -11,8 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 public class JwtUtil {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
+    @Value("${jwt.secret}")
     private String jwtSecret;
+    @Value("${jwt.expiration.ms}")
     private int jwtExpirationMs;
+    @Value("${jwt.cookie.name}")
     private String jwtCookie;
 
     public String getJwtFromCookies(HttpServletRequest request){
