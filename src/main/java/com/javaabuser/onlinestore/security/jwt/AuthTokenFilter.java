b24251ca921342ef4531438d2bbd87aed20e80de
JavaAbuser <steblevskiymaxim@ua.fm>
2022-08-01 16:08:44 +0300

    AuthTokenFilter was created

---
+++ src/main/java/com/javaabuser/onlinestore/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,58 @@
+package com.javaabuser.onlinestore.security.jwt;
+
+import com.javaabuser.onlinestore.security.CustomerDetails;
+import com.javaabuser.onlinestore.services.CustomerDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    private final CustomerDetailsService customerDetailsService;
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public AuthTokenFilter(CustomerDetailsService customerDetailsService, JwtUtil jwtUtil) {
+        this.customerDetailsService = customerDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtil.validateJwtToken(jwt)) {
+                String username = jwtUtil.getUserNameFromJwtToken(jwt);
+                CustomerDetails customerDetails = (CustomerDetails) customerDetailsService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(customerDetails,
+                                null,
+                                customerDetails.getAuthorities());
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.error("Cannot set user authentication: {}", e);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String jwt = jwtUtil.getJwtFromCookies(request);
+        return jwt;
+    }
+}
