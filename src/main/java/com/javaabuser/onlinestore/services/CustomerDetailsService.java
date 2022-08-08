b935f96c7ee5bc68c4a50c82a22e2baaec84d3bc
JavaAbuser <steblevskiymaxim@ua.fm>
2022-08-08 17:19:32 +0300

    Added annotation Transactional

--- src/main/java/com/javaabuser/onlinestore/services/CustomerDetailsService.java
+++ src/main/java/com/javaabuser/onlinestore/services/CustomerDetailsService.java
@@ -8,8 +8,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class CustomerDetailsService implements UserDetailsService {
 
     private final CustomerRepository customerRepository;
