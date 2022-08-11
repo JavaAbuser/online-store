9cf0965be999ab7bcdaa9d4f6520e012ddec68dd
JavaAbuser <steblevskiymaxim@ua.fm>
2022-08-11 23:20:06 +0300

    Added Query annotation

--- src/main/java/com/javaabuser/onlinestore/repositories/CustomerRepository.java
+++ src/main/java/com/javaabuser/onlinestore/repositories/CustomerRepository.java
@@ -2,12 +2,15 @@ package com.javaabuser.onlinestore.repositories;
 
 import com.javaabuser.onlinestore.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    Optional<Customer> findByEmail(String customerName);
+    @Query("SELECT customer.email FROM Customer customer WHERE customer.email = :email")
+    Optional<Customer> findByEmail(@Param("email") String customerEmail);
     Boolean existsCustomerByEmail(String customerEmail);
 }
