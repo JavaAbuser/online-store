97df03bb75ee908b15b3e0168721a02e8464e293
JavaAbuser <steblevskiymaxim@ua.fm>
2022-08-27 12:32:51 +0300

    Created entity moderator

---
+++ src/main/java/com/javaabuser/onlinestore/models/Moderator.java
@@ -0,0 +1,58 @@
+package com.javaabuser.onlinestore.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "moderator")
+public class Moderator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+    
+    @Column(name = "email")
+    private String email;
+
+    public Moderator() {
+    }
+
+    public Moderator(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
