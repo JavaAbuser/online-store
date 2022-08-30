d51e44546a27810f22407b85d07a9e26c631b45d
JavaAbuser <steblevskiymaxim@ua.fm>
2022-08-30 23:35:37 +0300

    Created Product.java

---
+++ src/main/java/com/javaabuser/onlinestore/models/Product.java
@@ -0,0 +1,46 @@
+package com.javaabuser.onlinestore.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "price")
+    private int price;
+    @Column(name = "description")
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
