6adc5ca7511c7d75d3725b641baf966104a11664
JavaAbuser <steblevskiymaxim@ua.fm>
2022-09-12 15:20:27 +0300

    Created CategoryNotFoundException

---
+++ src/main/java/com/javaabuser/onlinestore/exceptions/category/CategoryNotFoundException.java
@@ -0,0 +1,23 @@
+package com.javaabuser.onlinestore.exceptions.category;
+
+public class CategoryNotFoundException extends RuntimeException{
+    public CategoryNotFoundException() {
+        super("Category not found.");
+    }
+
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+
+    public CategoryNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CategoryNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected CategoryNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
