0f52c2738d4c1cc529a0d18e024cddb19f23d878
Max <steblevskiymaxim@ua.fm>
2023-01-13 16:54:08 +0200

    changed class name from Customer to User

--- src/main/java/com/javaabuser/onlinestore/models/User.java
+++ src/main/java/com/javaabuser/onlinestore/models/User.java
@@ -1,51 +1,59 @@
 package com.javaabuser.onlinestore.models;
 
 import com.javaabuser.onlinestore.models.enums.ERole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Collection;
 import java.util.Objects;
 
 @Entity
-@Table(name = "customer")
-public class Customer {
+@Table(name = "user")
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
-    @NotNull
-    @Size(max = 30)
+//    @NotNull
+//    @Size(max = 30)
     @Column(name = "name")
     private String name;
 
-    @NotNull
+//    @NotNull
     @Column(name = "password")
     private String password;
 
-    @Email
-    @NotNull
-    @Size(max = 40)
+//    @Email
+//    @NotNull
+//    @Size(max = 40)
     @Column(name = "email")
     private String email;
 
-    @Transient
-    private Role role;
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private ERole role;
 
-    public Customer() {
+    public User() {
     }
 
-    public Customer(String name, String password, String email) {
+    public User(String name, String password, String email, ERole role) {
         this.name = name;
         this.password = password;
         this.email = email;
-        this.role = new Role(ERole.CUSTOMER);
+        this.role = role;
     }
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -56,8 +64,38 @@ public class Customer {
         this.name = name;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
@@ -72,20 +110,20 @@ public class Customer {
         this.email = email;
     }
 
-    public Role getRole() {
+    public ERole getRole() {
         return role;
     }
 
-    public void setRole(Role role) {
+    public void setRole(ERole role) {
         this.role = role;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Customer)) return false;
-        Customer customer = (Customer) o;
-        return id == customer.id && name.equals(customer.name) && password.equals(customer.password) && email.equals(customer.email) && role.equals(customer.role);
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id && name.equals(user.name) && password.equals(user.password) && email.equals(user.email) && role == user.role;
     }
 
     @Override
