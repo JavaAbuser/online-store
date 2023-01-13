7a3dbf2966c40a2ae028ac1b78ae9e872486fec4
Max <steblevskiymaxim@ua.fm>
2023-01-13 16:47:36 +0200

    updated

--- src/main/java/com/javaabuser/onlinestore/configuration/SecurityConfiguration.java
+++ src/main/java/com/javaabuser/onlinestore/configuration/SecurityConfiguration.java
@@ -1,52 +1,54 @@
 package com.javaabuser.onlinestore.configuration;
 
 import com.javaabuser.onlinestore.security.jwt.AuthTokenFilter;
+import com.javaabuser.onlinestore.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableJpaRepositories("com.javaabuser.onlinestore.repositories")
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration{
+    private final AuthenticationProvider authenticationProvider;
+    private final AuthTokenFilter authTokenFilter;
+
+    public SecurityConfiguration(AuthenticationProvider authenticationProvider, AuthTokenFilter authTokenFilter1) {
+        this.authenticationProvider = authenticationProvider;
+        this.authTokenFilter = authTokenFilter1;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .antMatchers("/auth/register", "/auth/login").permitAll()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .authorizeRequests().antMatchers("/customer/register", "/customer/login").permitAll()
-                .anyRequest().authenticated();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
-        http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Bean
-    public AuthTokenFilter authTokenFilter(){
-        return new AuthTokenFilter();
+        return http.build();
     }
 }
