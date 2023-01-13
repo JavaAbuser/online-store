e93b3b014c00768fb6ebf4d8bbfe50f2acc32fcc
Max <steblevskiymaxim@ua.fm>
2023-01-13 16:49:08 +0200

    delegated functionality to authenticationService

--- src/main/java/com/javaabuser/onlinestore/controllers/AuthController.java
+++ src/main/java/com/javaabuser/onlinestore/controllers/AuthController.java
@@ -1,102 +1,54 @@
 package com.javaabuser.onlinestore.controllers;
 
-import com.javaabuser.onlinestore.exceptions.CustomerAlreadyExistsException;
+import com.javaabuser.onlinestore.auth.AuthenticationService;
 import com.javaabuser.onlinestore.exceptions.InvalidInputException;
-import com.javaabuser.onlinestore.exceptions.RoleNotFoundException;
-import com.javaabuser.onlinestore.models.Customer;
-import com.javaabuser.onlinestore.models.Role;
-import com.javaabuser.onlinestore.models.enums.ERole;
-import com.javaabuser.onlinestore.payload.LoginRequest;
-import com.javaabuser.onlinestore.payload.RegisterRequest;
-import com.javaabuser.onlinestore.security.CustomerDetails;
-import com.javaabuser.onlinestore.security.jwt.JwtUtil;
-import com.javaabuser.onlinestore.services.CustomerService;
-import com.javaabuser.onlinestore.services.RoleService;
+import com.javaabuser.onlinestore.auth.payload.LoginRequest;
+import com.javaabuser.onlinestore.auth.payload.RegisterRequest;;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/auth")
 public class AuthController {
-    private final CustomerService customerService;
-    private final RoleService roleService;
-    private final AuthenticationManager authenticationManager;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtUtil jwtUtil;
+    private final AuthenticationService authenticationService;
 
     @Autowired
-    public AuthController(CustomerService customerService, RoleService roleService, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
-        this.customerService = customerService;
-        this.roleService = roleService;
-        this.authenticationManager = authenticationManager;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtUtil = jwtUtil;
+    public AuthController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody @Valid RegisterRequest registerRequest, BindingResult bindingResult){
-        if (bindingResult.hasErrors())
-        {
-            StringBuilder bindingErrorsMessage = new StringBuilder();
+    public ResponseEntity<?> register(@RequestBody RegisterRequest registerRequest){
+//        if (bindingResult.hasErrors())
+//        {
+//            StringBuilder bindingErrorsMessage = new StringBuilder();
+//
+//            bindingResult
+//                    .getFieldErrors()
+//                    .forEach(field -> bindingErrorsMessage.append(field.getField()).append(": ").append(field.getDefaultMessage()));
+//
+//            throw new InvalidInputException(bindingErrorsMessage.toString());
+//        }
 
-            bindingResult
-                    .getFieldErrors()
-                    .forEach(field -> bindingErrorsMessage.append(field.getField()).append(": ").append(field.getDefaultMessage()));
-
-            throw new InvalidInputException(bindingErrorsMessage.toString());
-        }
-
-        if(customerService.existsCustomerByEmail(registerRequest.getEmail())){
-            throw new CustomerAlreadyExistsException();
-        }
-
-        Customer customer = new Customer(registerRequest.getName(),
-                passwordEncoder.encode(registerRequest.getPassword()),
-                registerRequest.getEmail());
-
-        Role role = roleService.findByRole(ERole.CUSTOMER)
-                .orElseThrow(() -> new RoleNotFoundException("Role " + ERole.CUSTOMER.name() + " not found."));
-
-        customer.setRole(role);
-        customerService.save(customer);
-
-        return ResponseEntity.ok(customer);
+        return ResponseEntity.ok(authenticationService.register(registerRequest));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> authenticate(@RequestBody @Valid LoginRequest loginRequest){
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        CustomerDetails customerDetails = (CustomerDetails) authentication.getPrincipal();
-
-        ResponseCookie jwtCookie = jwtUtil.generateJwtCookie(customerDetails);
-
-        Role role = roleService.findByRole(ERole.CUSTOMER)
-                .orElseThrow(() -> new RoleNotFoundException("Role " + ERole.CUSTOMER.name() + " not found."));
-
-        return ResponseEntity.ok(jwtCookie);
+    public ResponseEntity<?> authenticate(@RequestBody LoginRequest loginRequest){
+        return ResponseEntity.ok(authenticationService.authenticate(loginRequest));
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<HttpStatus> logout(){
-        return null;
+    public HttpStatus logout(HttpServletRequest request){
+        authenticationService.logout(request);
+        return HttpStatus.OK;
     }
 
     @PostMapping("/registration")
