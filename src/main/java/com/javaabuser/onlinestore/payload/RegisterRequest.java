08a36e6a9f909898b73105cb81792212eafdcf18
Max <steblevskiymaxim@ua.fm>
2023-01-13 16:34:26 +0200

    changed package and commented smt

--- src/main/java/com/javaabuser/onlinestore/payload/RegisterRequest.java
+++ src/main/java/com/javaabuser/onlinestore/payload/RegisterRequest.java
@@ -1,29 +1,45 @@
-package com.javaabuser.onlinestore.payload;
+package com.javaabuser.onlinestore.auth.payload;
 
+import com.javaabuser.onlinestore.models.enums.ERole;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class RegisterRequest {
-    @NotNull
-    @Size(max = 30)
+//    @NotNull
+//    @Size(max = 30)
     private String name;
 
-    @NotNull
+//    @NotNull
     private String password;
 
-    @Email
-    @NotNull
-    @Size(max = 40)
+//    @Email
+//    @NotNull
+//    @Size(max = 40)
     private String email;
 
-    public RegisterRequest(String name, String password, String email) {
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+
+    public RegisterRequest(String name, String password, String email, ERole role) {
         this.name = name;
         this.password = password;
         this.email = email;
+        this.role = role;
     }
 
     public RegisterRequest() {
+    }
+
+    public ERole getRole() {
+        return role;
+    }
+
+    public void setRole(ERole role) {
+        this.role = role;
     }
 
     public String getName() {
